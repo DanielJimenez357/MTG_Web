0468b9cb57326984f342cc857f15940948045a82
Mayonesa357 <djimmor2012@g.educaand.es>
2025-01-19 02:29:07 +0100

    ajuste del terraform, eleminacion de clave pem en local

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,19 +8,6 @@ resource "aws_key_pair" "deployer" {
   public_key = tls_private_key.example.public_key_openssh
 }
 
-
-resource "null_resource" "fix_permissions" {
-  depends_on = [local_file.private_key]
-
-  provisioner "local-exec" {
-    command = <<EOT
-    powershell -Command "icacls ${path.module}\\deployer-key.pem /inheritance:r"
-    powershell -Command "icacls ${path.module}\\deployer-key.pem /grant:r \"$($env:USERNAME):(F)\""
-    powershell -Command "icacls ${path.module}\\deployer-key.pem /remove \"BUILTIN\\Users\""
-    EOT
-  }
-}
-
 resource "aws_security_group" "allow_ssh_http" {
   vpc_id = aws_vpc.main.id
 
@@ -62,9 +49,3 @@ resource "aws_instance" "web" {
               systemctl enable apache2
               EOF
 }
-
-resource "local_file" "private_key" {
-  content  = tls_private_key.example.private_key_pem
-  filename = "${path.module}/deployer-key.pem"
-}
-
