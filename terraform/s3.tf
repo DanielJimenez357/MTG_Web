3a47f8cb392808e5522455241976c57cef9e23bf
Mayonesa357 <djimmor2012@g.educaand.es>
2025-01-27 19:57:51 +0100

    fallo ortografico resuelto 2

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,6 +6,6 @@ resource "aws_s3_object" "subir_archivos" {
   for_each = { for file in fileset("../web/", "**/*") : file => file }
   bucket   = aws_s3_bucket.mi_bucket.bucket
   key      = each.value
-  source   = "../web${each.value}"
+  source   = "../web/${each.value}"
 }
 
